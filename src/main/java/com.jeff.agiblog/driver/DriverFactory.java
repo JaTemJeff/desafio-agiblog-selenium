1e9f02c55216c1a46e5538e208417acb17f3ed5c
Jeff <jff_94@hotmail.com>
2026-03-25 12:08:38 -0300

    rollback no DriverFactory

--- src/main/java/com.jeff.agiblog/driver/DriverFactory.java
+++ src/main/java/com.jeff.agiblog/driver/DriverFactory.java
@@ -3,23 +3,10 @@ package com.jeff.agiblog.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
     public static WebDriver createDriver() {
-
-        ChromeOptions options = new ChromeOptions();
-
-        boolean isCI = System.getenv("CI") != null;
-
-        if (isCI) {
-            options.addArguments("--headless=new");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--window-size=1920,1080");
-            options.addArguments("--remote-allow-origins=*");
-        }
-
-        return new ChromeDriver(options);
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
     }
 }
