53348a84f00bcbe92da4cc62fcfec48806b6ee4e
Jeff <jff_94@hotmail.com>
2026-03-25 12:24:06 -0300

    Corrigindo driver factory

--- src/main/java/com.jeff.agiblog/driver/DriverFactory.java
+++ src/main/java/com.jeff.agiblog/driver/DriverFactory.java
@@ -1,12 +1,24 @@
 package com.jeff.agiblog.driver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
+
     public static WebDriver createDriver() {
-        WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+
+        boolean isCI = System.getenv("CI") != null;
+
+        if (isCI) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        return new ChromeDriver(options);
     }
 }
